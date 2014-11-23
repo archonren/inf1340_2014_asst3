cb3d987f208a0fb04556d010bab51ba9c9c7427c
archonren <archon_ren@hotmail.com>
2014-11-22 19:06:02 -0500

    first commit. locate all data to right place.

--- mining.py
+++ mining.py
@@ -2,24 +2,53 @@
 
 """ Docstring """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Paul and Archon'
+__email__ = "guanhua.ren@mail.utoronto.ca"
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Paul and Archon"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
 
 # imports one per line
 import json
-import datetime
+from datetime import *
+
 
 stock_data = []
 monthly_averages = []
 
 
+def read_json_from_file(file_name):
+    with open(file_name) as file_handle:
+        file_contents = file_handle.read()
+    return json.loads(file_contents)
+
+
 def read_stock_data(stock_name, stock_file_name):
-    return
+    stock_data_list = read_json_from_file(stock_file_name)
+    date_list = []
+    for day_stock_price_detail in stock_data_list:
+        stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
+        date_list.append(stock_date)
+    date_list = sorted(date_list)
+    year_range = []
+    for year in range(date_list[0].year, date_list[-1].year+1):
+        year_range.append(year)
+  #find the range of year#
+    year_dict = {}
+    for year in year_range:
+        year_dict[year] = [[], [], [], [], [], [], [], [], [], [], [], [], []]
+    for day_stock_price_detail in stock_data_list:
+        stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
+        stock_month = stock_date.month
+        stock_year = stock_date.year
+        year_dict[stock_year][stock_month] = day_stock_price_detail
+  #year_dict has key as year, and value as stock detail#
+
+
+
+
 
 
 def six_best_months():
@@ -30,9 +59,4 @@ def six_worst_months():
     return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
 
 
-def read_json_from_file(file_name):
-    with open(file_name) as file_handle:
-        file_contents = file_handle.read()
-
-    return json.loads(file_contents)
-
+read_stock_data("GOOG", "data/GOOG.json")

4e1a4e45db75f92a17e72059eaa0a2f80323412b
archonren <archon_ren@hotmail.com>
2014-11-25 17:41:45 -0500

    delete a line

--- mining.py
+++ mining.py
@@ -37,8 +37,7 @@ def read_stock_data(stock_name, stock_file_name):
     for day_stock_price_detail in stock_data_list:
         stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
         day_stock_price_detail["Date"] = str(stock_date.year)+"/"+str(stock_date.month)
-        year_dict.setdefault(day_stock_price_detail["Date"])
-        year_dict[day_stock_price_detail["Date"]] = [0, 0]
+        year_dict.setdefault(day_stock_price_detail["Date"], [0, 0])
   #fix the date, delete the day, only leave the year and month and set up space for sales and volume#
     for day_stock_price_detail in stock_data_list:
         year_dict[day_stock_price_detail["Date"]][0] += day_stock_price_detail["Close"]*day_stock_price_detail["Volume"]

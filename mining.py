2943bb0953b7b8dd184e27e997444c05a366048c
archonren <archon_ren@hotmail.com>
2014-11-30 02:03:02 -0500

    fix error

--- mining.py
+++ mining.py
@@ -22,6 +22,9 @@ def read_json_from_file(file_name):
         file_contents = file_handle.read()
     return json.loads(file_contents)
 
+global avg_price
+avg_price = []
+
 
 def read_stock_data(stock_name, stock_file_name):
     """
@@ -33,10 +36,12 @@ def read_stock_data(stock_name, stock_file_name):
     """
     stock_data_list = read_json_from_file(stock_file_name)
     year_dict = {}
+    global avg_price
     avg_price = []
+    clear_incomplete_month(stock_data_list)
     for day_stock_price_detail in stock_data_list:
         stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
-        day_stock_price_detail["Date"] = str(stock_date.year)+"/"+str(stock_date.month)
+        day_stock_price_detail["Date"] = str(stock_date.year)+"/"+str(stock_date.month).zfill(2)
         year_dict.setdefault(day_stock_price_detail["Date"], [0, 0])
   #fix the date, delete the day, only leave the year and month and set up space for sales and volume#
     for day_stock_price_detail in stock_data_list:
@@ -47,12 +52,28 @@ def read_stock_data(stock_name, stock_file_name):
         price = year_dict[key_time][0]/year_dict[key_time][1]
         price = Decimal(price).quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
         avg_price.append((key_time, float(price)))
-    return avg_price
 
 
 def six_best_months():
-    return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
+    outcome = sorted(avg_price, key=lambda x: x[1], reverse=True)
+    print(outcome[:6:])
+    return outcome[:6:]
 
 
 def six_worst_months():
-    return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
+    outcome = sorted(avg_price, key=lambda x: x[1])
+    print(outcome[:6:])
+    return outcome[:6:]
+
+"""
+def clear_incomplete_month(stock_data_list):
+    stock_year_month = []
+    stock_data_list = sorted(stock_data_list, key=lambda x: x["Date"])
+    for day_stock_price_detail in stock_data_list:
+        stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
+        stock_year_month.append(str(stock_date.year)+"/"+str(stock_date.month).zfill(2))
+    stock_year_month = list(set(stock_year_month))
+    for day_stock_price_detail in stock_data_list:
+        stock_date = datetime.strptime(day_stock_price_detail["Date"], "%Y-%m-%d")
+
+"""
